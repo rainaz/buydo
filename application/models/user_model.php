b184d477f801a9ea924775e0c6dd68553c9f2bd4
Sarin.Du <sarin.durongdamrongchai@gmail.com>
2014-11-19 14:18:56 +0700

    add empty model file

---
+++ application/models/user_model.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class Person extends CI_Controller {
+	function __construct(){
+		parrent::__construct();
+	}
+	function add_user($name, $surname, )
+
+}
+?>
