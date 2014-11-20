824143d4e6ef6999d9ead1b64edc0c3b506ab7eb
Noppayut <noppayut@hotmail.com>
2014-11-20 21:22:40 +0700

    Nuttt's user model

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,199 +1,145 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 class User_Model extends CI_Model {
 
-	private $_userID;
-	private $_name;
-	private $_surname;
-	private $_creditcard;
-	private $_birthday;
-	private $_country;
-	private $_sentAddress;
-	private $_address;
-	private $_username;
-	private $_password;
-	private $_phoneNo;
-	private $_bannedby;
-	private $_startBanned;
-	private $_bannedDuration;
-	private $_bannedReason;
-	private $_penaltyCount;
+	private $personal="userID, name, surname, email, creditcard, birthday, country,sentAddress,address";
+	private $account = "username, password, phoneNo";
+	private $ban = "bannedby, startBanned, bannedDuration, bannedReason, penaltyCount"
+	private $attributes = $personal.$account.$ban;
+
 
 	function __construct(){
-		parrent::__construct();
-	}
-
-
-	//commit the object in php to a tuple in database
-
-	public function commit(){
-		$data = array(
-			'userID' => $this->_userID;
-			'name' => $this->_name;
-			'surname' => $this->_surname;
-			'creditcard' => $this->_creditcard;
-			'birthday' => $this->_birthday;
-			'country' => $this->_country;
-			'sentAddress' => $this->_sentAddress;
-			'address' => $this->_address;
-			'username' => $this->_username;
-			'password' => $this->_password;
-			'phoneNo' => $this->_phoneNo;
-			'bannedby' => $this->_bannedby;
-			'startBanned' => $this->_startBanned;
-			'bannedDuration' => $this->_bannedDuration;
-			'bannedReason' => $this->_bannedReason;
-			'penaltyCount' => $this->_penaltyCount;
-			);
-
-		if($this->_userID > 0){	//user already exists, just update user info
-			if ($this->db->update("User", $data, array("userID" => $this->_userID))) {
-				return true;
-			}
-
-		}
-		else {	//user doesn't exist, create new user
-			if ($this->db->insert("User", $data)) {
-				//Now we can get the ID and update the newly created object
-				$this->_userID = $this->db->insert_id();
-				return true;
-			}
-
-		}
+		parent::__construct();
 	}
 
 	// this function is to be moved to FACTORY	
-	function add_user($name, $surname, $creditcard=NULL, $birthday, $country
+	public function addUser($name, $surname, $email, $creditcard=NULL, $birthday, $country
 		, $sentAddress, $address, $username, $password, $phoneNo
 		, $bannedby, $startBanned, $bannedDuration, $bannedReason, $penaltyCount){
+		$lastrow = $this->db->insert_id();		
+		$insvalue = "('".$lastrow."', '".
+			$name."', '".
+			$surname."', '".
+			$email."', '".
+			$creditcard."', '".
+			$birthday."', '".
+			$country."', '".
+			$sentAddress."', '".
+			$address."', '".
+			$username."', '".
+			$password."', '".
+			$phoneNo."', '".
+			$startBanned."', '".
+			$bannedDuration."', '".
+			$bannedReason."', '".
+			$penaltyCount."')";
+		$sql = "INSERT INTO user ($attributes) values ".$insvalue;
 
+		if(/* no email */)
+		$query = $this->db->query($sql);
+
+		if($query->num_rows() > 0){
+			return $query->row();
+		}
+		return false;
 
 	}
 
-	public function getUserID(){
-		return $this->_userID;
+	public function verifyUserExistByEmail($email){
+		$sql = "SELECT email FROM user WHERE email = "."'".$email."'";		
+		$query = $this->db->query($sql);
+		if($query->num_rows() > 0){
+			return true;
+		}
+		return false;
 	}
 
-	public function setUserID($userid){
-		$this->_userID = $userid;
+	public function getLastRow(){
+		return $this->db->insert_id();
 	}
 
-	public function getName(){
-		return $this->_name;
+	public function getUserIDByEmail($email){
+		$query = $this->db->query("SELECT userID FROM user WHERE email = '".$email."'");
+		if($query->num_rows() > 0){
+			return $query->row()->email;
+		}
+		return false;
 	}
 
-	public function setName($name){
-		$this->_name = $name;
+	public function getUserByUserID($id){
+		$query = $this->db->query("SELECT $this->attributes FROM user WHERE userID = '".$id."'");
+		if($query->num_rows() > 0){
+			return $query->row();
+		}
+		return false;
 	}
 
-	public function getSurname(){
-		return $this->_surname;
+	public function setNameByUserID($id, $nname){
+		$sql = "UPDATE user SET name = "."'".$nname."'"."WHERE userID = "."'".$id."'";
+		$query = $this->db->query($sql);		
 	}
 
-	public function setSurname($surname){
-		$this->_surname = $surname;
-	}	
-
-	public function getCreditcard(){
-		return $this->_creditcard;
+	public function setSurnameByUserID($id, $nsurname){
+		$sql = "UPDATE user SET surname = "."'".$nsurname."'"."WHERE userID = "."'".$id."'";
+		$query = $this->db->query($sql);	
 	}
 
-	public function setCreditcard($creditcard){
-		$this->_creditcard = $creditcard;
-	}	
 
-	public function getBirthday(){
-		return $this->_birthday;
-	}
+	// private $_userID;
+	// private $_name;
+	// private $_surname;
+	// private $_creditcard;
+	// private $_birthday;
+	// private $_country;
+	// private $_sentAddress;
+	// private $_address;
+	// private $_username;
+	// private $_password;
+	// private $_phoneNo;
+	// private $_bannedby;
+	// private $_startBanned;
+	// private $_bannedDuration;
+	// private $_bannedReason;
+	// private $_penaltyCount;
 
-	public function setBirthday($bd){
-		$this->_birthday = $bd;
-	}	
+		//commit the object in php to a tuple in database
 
-	public function getCountry(){
-		return $this->_country;
-	}
+	// public function commit(){
+	// 	$data = array(
+	// 		'userID' => $this->_userID;
+	// 		'name' => $this->_name;
+	// 		'surname' => $this->_surname;
+	// 		'creditcard' => $this->_creditcard;
+	// 		'birthday' => $this->_birthday;
+	// 		'country' => $this->_country;
+	// 		'sentAddress' => $this->_sentAddress;
+	// 		'address' => $this->_address;
+	// 		'username' => $this->_username;
+	// 		'password' => $this->_password;
+	// 		'phoneNo' => $this->_phoneNo;
+	// 		'bannedby' => $this->_bannedby;
+	// 		'startBanned' => $this->_startBanned;
+	// 		'bannedDuration' => $this->_bannedDuration;
+	// 		'bannedReason' => $this->_bannedReason;
+	// 		'penaltyCount' => $this->_penaltyCount;
+	// 		);
 
-	public function setCountry($country){
-		$this->_country = $country;
-	}	
+	// 	if($this->_userID > 0){	//user already exists, just update user info
+	// 		if ($this->db->update("User", $data, array("userID" => $this->_userID))) {
+	// 			return true;
+	// 		}
 
-	public function getSentAddress(){
-		return $this->_sentAddress;
-	}
+	// 	}
+	// 	else {	//user doesn't exist, create new user
+	// 		if ($this->db->insert("User", $data)) {
+	// 			//Now we can get the ID and update the newly created object
+	// 			$this->_userID = $this->db->insert_id();
+	// 			return true;
+	// 		}
 
-	public function setSentAddress($sa){
-		$this->_sentAddress = $sa;
-	}	
+	// 	}
+	// }
 
-	public function getAddress(){
-		return $this->_address;
-	}
 
-	public function setAddress($address){
-		$this->_address = $address;
-	}	
-
-	public function getUsername(){
-		return $this->_username;
-	}
-
-	public function setUsername($username){
-		$this->_username = $username;
-	}	
-
-	public function getPassword(){
-		return $this->_password;
-	}
-
-	public function setName($passwd){
-		$this->_password = $passwd;
-	}	
-
-	public function getPhoneNo(){
-		return $this->_phoneNo;
-	}
-
-	public function setPhoneNo($phoneNo){
-		$this->_phoneNo = $phoneNo;
-	}	
-
-	public function getBannedBy(){
-		return $this->_bannedby;
-	}
-
-	public function setBannedBy($bannedby){
-		$this->_bannedby = $bannedby;
-	}
-
-	public function getStartBanned(){
-		return $this->_startBanned;
-	}
-
-	public function setStartBanned($startban){
-		$this->_startBanned = $startban;
-	}
-	public function getBannedDuration(){
-		return $this->_bannedDuration;
-	}
-
-	public function setBannedBy($bannedduration){
-		$this->_bannedDuration = $bannedduration;
-	}
-	public function getBannedReason(){
-		return $this->_bannedReason;
-	}
-
-	public function setBannedBy($bannedreason){
-		$this->_bannedReason = $bannedreason;
-	}
-
-	public function getPenaltyCount(){
-		return $this->_penaltyCount;
-	}
-
-	public function setPenaltyCount($penaltyCount){
-		$this->_penaltyCount = $penaltyCount;
-	}
 
 }
 ?>
