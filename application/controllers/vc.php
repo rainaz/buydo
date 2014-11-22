1e55615dcd6b93d0f28789c36e6ffd63a018f644
Sarin.Du <sarin.durongdamrongchai@gmail.com>
2014-11-23 02:13:54 +0700

    beutify code user and vc controller

--- application/controllers/vc.php
+++ application/controllers/vc.php
@@ -2,8 +2,7 @@
 class VC extends CI_Controller {
 
 	// Access this controller from http://localhost/buydo/index.php/vc/
-	public function index($page)
-	{
+	public function index($page){
 		$this->load->view('sample_view/main.html');
 	}
 
@@ -11,8 +10,7 @@ class VC extends CI_Controller {
 	// i.e. http://localhost/buydo/index.php/vc/show/content/sample
 	
 
-	public function show($folder, $page)
-	{
+	public function show($folder, $page){
 		if($folder=='user'||$folder=='seller') $data['template_type'] = "corperate";
 		else $data['template_type'] = "ecommerce";
 		$this->load->view('header/header', $data);
@@ -29,35 +27,33 @@ class VC extends CI_Controller {
       // $this->load->view('footer');
 	}
 	public function sendEmail(){
-		 $config = Array(
-  'protocol' => 'smtp',
-  'smtp_host' => 'ssl://smtp.googlemail.com',
-  'smtp_port' => 465,
-  'smtp_user' => 'pladookspiral@gmail.com', // change it to yours
-  'smtp_pass' => 'pladookmasta', // change it to yours
-  'mailtype' => 'html',
-  'charset' => 'iso-8859-1',
-  'wordwrap' => TRUE
-);
+		$config = Array(
+			'protocol' => 'smtp',
+			'smtp_host' => 'ssl://smtp.googlemail.com',
+			'smtp_port' => 465,
+			'smtp_user' => 'pladookspiral@gmail.com', // change it to yours
+			'smtp_pass' => 'pladookmasta', // change it to yours
+			'mailtype' => 'html',
+			'charset' => 'iso-8859-1',
+			'wordwrap' => TRUE
+		);
 
         $message = 'HELLO BUYDOER LOLOLOLOL';
         $this->load->library('email', $config);
-      $this->email->set_newline("\r\n");
-      $this->email->from('pladookspiral@gmail.com'); // change it to yours
-      $this->email->to('nontawat.mk@gmail.com');// change it to yours
-      $this->email->subject('There is one secret in this world');
-      $this->email->message($message);
-      if($this->email->send())
-     {
-      echo 'Email sent.';
-     }
-     else
-    {
-     show_error($this->email->print_debugger());
+		$this->email->set_newline("\r\n");
+		$this->email->from('pladookspiral@gmail.com'); // change it to yours
+		$this->email->to('nontawat.mk@gmail.com');// change it to yours
+		$this->email->subject('There is one secret in this world');
+		$this->email->message($message);
+		if($this->email->send(){
+			echo 'Email sent.';
+		 }
+		else{
+			show_error($this->email->print_debugger());
     
-}
+		}
 	}
 
-
 }
+
 ?>
