3d43bc47ec953dc76bacb83ad06b324de2375f0b
AbatrozzProgrammingSpace <abatrozz@hotmail.com>
2014-11-23 22:35:08 +0700

    add new file in checkout folder

---
+++ application/views/checkout/buy_complete.php
@@ -0,0 +1,11 @@
+<!-- use corporate header -->
+<div class="main">
+	<div class="container">
+		<div class="row">
+		<img src="/buydo/assets/images/green_check.png"
+		</div>
+		<p><h1>Thank you for purchasing with Buydo! </h1></p>
+		<p><h2>You can check your transaction in your email. </h2></p>
+		
+	</div>
+</div>
