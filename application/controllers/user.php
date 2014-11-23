144e30f038f07d64329f41de00c7cee2d5bb83d4
Nolfwin Mythknight <you@example.com>
2014-11-23 15:00:48 +0700

    manage profile

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -8,6 +8,7 @@ class User extends CI_Controller{
 	public function index(){
 		if(($this->session->userdata('username')!="")){
 			$this->welcome();
+
 		}
 		else{
 			$data['title']= 'Home';
@@ -52,10 +53,29 @@ class User extends CI_Controller{
 			$this->thank();
 		}
 	}
+	public function manageprofle(){
+		$this->load->library('form_validation');
+		// field name, error message, validation rules
+		$this->form_validation->set_rules('user_name', 'User Name');
+		//  $this->form_validation->set_rules('email_address', 'Your Email', 'trim|required|valid_email');
+		//  $this->form_validation->set_rules('password', 'Password', 'trim|required|min_length[4]|max_length[32]');
+		//  $this->form_validation->set_rules('con_password', 'Password Confirmation', 'trim|required|matches[password]');
+		if($this->form_validation->run() == FALSE){
+			$this->index();
+		}
+		else{
+			$this->user_model->manageProfile();
+			$this->thank();
+		}
+	}
+
+
+
+
 	public function logout(){
 		$newdata = array(
 			'user_id'   =>'',
-			'username'  =>'',
+			'user_name'  =>'',
 			'user_email'     => '',
 			'logged_in' => FALSE,
 		);
