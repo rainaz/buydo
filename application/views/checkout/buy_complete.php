91d6488b4fcd9cc93393cae25ebd38f536db9d60
AbatrozzProgrammingSpace <abatrozz@hotmail.com>
2014-11-23 22:56:13 +0700

    add button to return to home page in buy_complete page

--- application/views/checkout/buy_complete.php
+++ application/views/checkout/buy_complete.php
@@ -11,6 +11,10 @@
 			<div class="padding-top-20 padding-bottom-20">
 			<p><h2>You can check your transaction in your email. </h2></p>
 			</div>
+			<div class="padding-top-20 padding-bottom-20">
+				<a href="../page/home.php"><button type="button" class="btn btn-primary">Done</button></a> 
+			</div>
+			<div class="padding-top-40"></div>
 		</div>
 	</div>
 </div>
