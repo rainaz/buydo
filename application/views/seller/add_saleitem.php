05b145e83cdf6881c5bc5766e13162c0b36bad84
AbatrozzProgrammingSpace <abatrozz@hotmail.com>
2014-11-24 01:09:11 +0700

    trial to use codeIgniter

--- application/views/seller/add_saleitem.php
+++ application/views/seller/add_saleitem.php
@@ -14,7 +14,7 @@
             <div class="content-form-page">
               <div class="row">
                 <div class="col-md-7 col-sm-7">
-                  <form class="form-horizontal" role="form" id="add_saleitem_form" action="item/submitSaleItem" method="post">
+                  <form class="form-horizontal" role="form" id="add_saleitem_form" action=<?php echo site_url('item/submitSaleItem_'); ?> method="post">
                     <fieldset>
                       <div class="form-group">
                         <label for="item_name" class="col-lg-4 control-label">Item name <span class="require">*</span></label>
@@ -37,7 +37,7 @@
                       <div class="form-group">
                         <label for="quantity_in_stock" class="col-lg-4 control-label">Quantity in stock <span class="require">*</span></label>
                         <div class="col-lg-8">
-                          <input type="number" min=0 class="form-control" id="quantity_in_stock" name="quantity_in_stock">
+                          <input type="number" min=0 class="form-control" id="quantity" name="quantity">
                         </div>
                       </div>
                     <div class="form-group">
