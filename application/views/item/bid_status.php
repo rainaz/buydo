f7a5d967e92bd478d52e3e2c1e6e991a8e6cc49e
Sarin Achawaranont <exmiraclelight@gmail.com>
2014-11-23 21:23:41 +0700

    add simple_message, bid_status, transaction_history view

--- application/views/item/bid_status.php
+++ application/views/item/bid_status.php
@@ -54,57 +54,6 @@
           <!-- PRODUCT ITEM END -->
         </div>
         <!-- END PRODUCT LIST -->
-
-        
-        <!-- BEGIN PRODUCT LIST -->
-        <div class="row product-list">
-          <!-- PRODUCT ITEM START -->
-          <div class="product-item claerfix">
-            <div class="col-md-3 col-sm-3 col-xs-12 text-center">
-              <img src="/buydo/assets/frontend/pages/img/products/model1.jpg" style="max-height: 200px; max-width: 300px;" alt="Berry Lace Dress">
-            </div>
-            <!-- PRODUCT ITEM END -->
-            <!-- PRODUCT ITEM START -->
-            <div class="col-md-9 col-sm-9 col-xs-12">
-              <div class="portlet" style="min">
-              <div class="portlet-title"><h2>ปลาดุกฟู</h2></div>
-              <div class="portlet-body" style="padding-bottom: 15px;">
-              <div class="pi-price" style="float: right;    padding-top: 19px;">Time Left 12:01</div>
-              <div class="pi-price" style="float: right;    padding-top: 19px; color: red;">Pay before 13/2</div>
-
-              <ul>
-
-                <li>Seller: นัท</li>
-                <li>Bid price: THB51.00</li>
-                <li>Bid ends at: 12:12 - 25/12/01</li>
-                <li>Bid status: in-progress</li>
-
-              </ul>
-            </div>
-                <button type="button" class="btn btn-primary"><i class="fa fa-money"></i> Pay</button>
-                <button type="button" class="btn btn-success"><i class="fa fa-share"></i> Bid</button>
-              <!--a href="#" class="icon-btn">
-                <i class="fa fa-money"></i>
-                <div>Pay</div>
-              </a>
-              <a href="#" class="icon-btn">
-                <i class="fa fa-bullhorn"></i>
-                <div>Complain Seller</div>
-              </a>
-              <a href="#" class="icon-btn">
-                <i class="fa fa-plane"></i>
-                <div>Notify Delivery</div>
-              </a>
-              <a href="#" class="icon-btn">
-                <i class="fa fa-thumbs-up"></i>
-                <div>Feedback</div>
-              </a-->
-            </div>
-          </div>
-          </div>
-          <!-- PRODUCT ITEM END -->
-        </div>
-        <!-- END PRODUCT LIST -->
         <!-- BEGIN PAGINATOR -->
         <div class="row">
           <div class="col-md-4 col-sm-4 items-info">Items 1 to 9 of 10 total</div>
