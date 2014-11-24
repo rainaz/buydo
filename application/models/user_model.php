841bfd59fd4978c2eb62f7cd53cfc452daff63a2
Noppayut <noppayut@hotmail.com>
2014-11-25 02:55:12 +0700

    change username to name

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -111,7 +111,7 @@ class User_model extends CI_Model {
       $narray = array(
         array(
           "user_id"=>$row['user_id'],
-          "name"=>$row['username'],
+          "name"=>$row['name'],
           "user_type"=>"Buyer"
         )
       );
@@ -121,7 +121,7 @@ class User_model extends CI_Model {
       $narray = array(
         array(
           "user_id"=>$row['user_id'],
-          "name"=>$row['username'],
+          "name"=>$row['name'],
           "user_type"=>"Seller"
         )
       );
