5b6c86aa98f006966920d50184979d79a36be04e
Nawapat <nawapat_earth@hotmail.com>
2014-11-24 20:57:50 +0700

    edit typo

--- application/views/checkout/bid_fail.php
+++ application/views/checkout/bid_fail.php
@@ -6,13 +6,13 @@
 				<img src="/buydo/assets/images/red_cross.jpg">
 			</div>
 			<div class="padding-top-40">
-			<p><h1>Thank you for bidding with Buydo!</h1></p>
+			<p><h1>Your bid is too low, Try again next time!</h1></p>
 			</div>
 			<div class="padding-top-20">
-			<p><h2>Your max bid is <?php echo $value ?> </h2></p>
+			<p><h2>Press back button to proceed again</h2></p>
 			</div>
 			<div class="padding-top-20">
-				<a href=<?php echo site_url('item/showItem/'.$item_id); ?>><button type="button" class="btn btn-primary">Done</button></a> 
+				<a href=<?php echo site_url('item/showItem/'.$item_id); ?>><button type="button" class="btn btn-primary">Back</button></a> 
 			</div>
 			<div class="padding-top-40"></div>
 		</div>
