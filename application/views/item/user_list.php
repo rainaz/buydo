594a536e191ea30de7ec2f48876bb42b4273a1fe
Sarin Achawaranont <exmiraclelight@gmail.com>
2014-11-25 01:10:51 +0700

    show user_list

--- application/views/item/user_list.php
+++ application/views/item/user_list.php
@@ -6,14 +6,14 @@
           <!-- BEGIN CONTENT -->
           <div class="col-md-12 col-sm-12">
             <h1>User List</h1>
-            <div class="well">
+            <!--div class="well">
               <h5> Display: </h5>
               <div class="btn-group btn-group-solid">
                   <button type="button" class="btn blue-madison">Buyer</button>
                   <button type="button" class="btn green-meadow">Seller</button>
                   <button type="button" class="btn blue-hoki">All</button>
                 </div>
-            </div>
+            </div-->
             <div class="goods-page">
               <div class="goods-data clearfix">
                 <div class="table-wrapper-responsive">
@@ -24,19 +24,27 @@
                     <th class="goods-page-description">Type</th>
 
                   </tr>
+
+                  <?php
+                    for ($i=0;$i<count($list);$i++) {
+                      $user = $list[$i];
+                  ?>
                   <tr>
                     <td class="goods-page-image">
-                      1
+                      <?php echo $user['user_id'] ?>
                     </td>
                     <td class="goods-page-description">
-                      <h3><a href="#">Username</a></h3>
+                      <h3><a href="<?php echo site_url('/transaction/showUserFeedback?userid='.$user['user_id']); ?>"><?php echo $user['name'] ?></a></h3>
 
                     </td>
                     <td class="goods-page-description">
-                      <p><strong>Buyer</strong><p>
+                      <p><strong><?php echo $user['user_type'] ?></strong><p>
 
                     </td>
                   </tr>
+                  <?php
+                    }
+                  ?>
                 </table>
                 </div>
 
