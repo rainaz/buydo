b8e30e501dcfb685a28a9f267673f67cb6bdba12
AbatrozzProgrammingSpace <abatrozz@hotmail.com>
2014-11-25 01:54:14 +0700

    add something cool

--- application/views/user/my_account.php
+++ application/views/user/my_account.php
@@ -1,9 +1,25 @@
 <div class="main">
 <div class="container">
 
-<ul class="list-unstyled list-inline">
-              <li><a href="<?php echo site_url('user/showManageProfilePage'); ?>">Manage Profile</a></li>
-              <li><a href="<?php echo site_url('user/transaction_history'); ?>">Transaction History</a></li>
-</ul>
+
+
+	<div class="sidebar col-md-4 col-sm-5">
+		<ul class="list-group margin-bottom-25 sidebar-menu">
+			<li class="list-group-item clearfix col-md-12 col-sm-12 padding-top-20 padding-bottom-20 paddind-right-10">
+				<a href="<?php echo site_url('user/showManageProfilePage'); ?>">
+					<i class="fa fa-angle-right fa-4x"></i> 
+						<h2>Manage Profile</h2>
+				</a>
+			</li>
+			<li class="list-group-item clearfix col-md-12 col-sm-12 padding-top-20 padding-bottom-20 paddind-right-10">
+				<a href="<?php echo site_url('user/transaction_history'); ?>">
+					<i class="fa fa-angle-right fa-4x"></i>
+					<h2>Transaction History</h2>
+				</a>
+			</li>
+
+		</ul>
+	</div>
+
 </div>
 </div>
