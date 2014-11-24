13f8d913a1ceffe2242cb961ef95ea2a995fb436
Sarin Achawaranont <exmiraclelight@gmail.com>
2014-11-24 16:14:01 +0700

    add user_list, user_info

---
+++ application/views/item/user_info.php
@@ -0,0 +1,79 @@
+
+    <div class="main">
+      <div class="container">
+        <!-- BEGIN SIDEBAR & CONTENT -->
+        <div class="row margin-bottom-40">
+
+          <!-- BEGIN CONTENT -->
+          <div class="col-md-12 col-sm-12">
+            <div class="product-page">
+              <div class="row">
+                <div class="col-md-12 col-sm-12">
+                  <h1>Username</h1>
+                  <div class="description">
+                    <p>Type: Seller</p>
+                  </div>
+                </div>
+
+                <div class="product-page-content">
+                  <ul id="myTab" class="nav nav-tabs">
+                    <li class="active"><a href="#Reviews" data-toggle="tab">Feedback</a></li>
+                  </ul>
+                  <div id="myTabContent" class="tab-content">
+                    <div class="tab-pane fade in active" id="Reviews">
+                      <!--<p>There are no reviews for this product.</p>-->
+                      <div class="review-item clearfix">
+                        <div style="float:right;">Rating: 2.0</div>
+
+                        <div class="review-item-submitted">
+                          <strong>Bob</strong>
+                          <em>30/12/2013 - 07:37</em>
+                          <div class="rateit" data-rateit-value="5" data-rateit-ispreset="true" data-rateit-readonly="true"></div>
+                        </div>                                              
+                        <div class="review-item-content">
+                            <p>Sed velit quam, auctor id semper a, hendrerit eget justo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel arcu pulvinar dolor tempus feugiat id in orci. Phasellus sed erat leo. Donec luctus, justo eget ultricies tristique, enim mauris bibendum orci, a sodales lectus purus ut lorem.</p>
+                        </div>
+                      </div>
+                      <div class="review-item clearfix">
+                        <div style="float:right;">Rating: 2.0</div>
+
+                        <div class="review-item-submitted">
+                          <strong>Mary</strong>
+                          <em>13/12/2013</em>
+                        </div>                                              
+                        <div class="review-item-content">
+                            <p>Sed velit quam, auctor id semper a, hendrerit eget justo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis vel arcu pulvinar dolor tempus feugiat id in orci. Phasellus sed erat leo. Donec luctus, justo eget ultricies tristique, enim mauris bibendum orci, a sodales lectus purus ut lorem.</p>
+                        </div>
+                      </div>
+
+<!--
+                      <form action="#" class="reviews-form" role="form">
+                        <h2>Add feedback</h2>
+                          <input type="hidden" id="name">
+
+                        <div class="form-group">
+                          <label for="review">Feedback <span class="require">*</span></label>
+                          <textarea class="form-control" rows="8" id="review"></textarea>
+                        </div>
+                        <div class="form-group">
+                          <label for="email">Rating</label>
+                          <input type="range" value="4" step="0.25" id="backing5">
+                          <div class="rateit" data-rateit-backingfld="#backing5" data-rateit-resetable="false"  data-rateit-ispreset="true" data-rateit-min="0" data-rateit-max="5">
+                          </div>
+                        </div>
+                        <div class="padding-top-20">                  
+                          <button type="submit" class="btn btn-primary">Send</button>
+                        </div>
+                      </form>
+--> 
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- END CONTENT -->
+        </div>
+        <!-- END SIDEBAR & CONTENT -->
+      </div>
+    </div>
---
+++ application/views/item/user_list.php
@@ -0,0 +1,50 @@
+
+    <div class="main">
+      <div class="container">
+        <!-- BEGIN SIDEBAR & CONTENT -->
+        <div class="row margin-bottom-40">
+          <!-- BEGIN CONTENT -->
+          <div class="col-md-12 col-sm-12">
+            <h1>User List</h1>
+            <div class="well">
+              <h5> Display: </h5>
+              <div class="btn-group btn-group-solid">
+                  <button type="button" class="btn blue-madison">Buyer</button>
+                  <button type="button" class="btn green-meadow">Seller</button>
+                  <button type="button" class="btn blue-hoki">All</button>
+                </div>
+            </div>
+            <div class="goods-page">
+              <div class="goods-data clearfix">
+                <div class="table-wrapper-responsive">
+                <table>
+                  <tr>
+                    <th class="goods-page-image">ID</th>
+                    <th class="goods-page-description">Username</th>
+                    <th class="goods-page-description">Type</th>
+
+                  </tr>
+                  <tr>
+                    <td class="goods-page-image">
+                      1
+                    </td>
+                    <td class="goods-page-description">
+                      <h3><a href="#">Username</a></h3>
+
+                    </td>
+                    <td class="goods-page-description">
+                      <p><strong>Buyer</strong><p>
+
+                    </td>
+                  </tr>
+                </table>
+                </div>
+
+              </div>
+            </div>
+          </div>
+          <!-- END CONTENT -->
+        </div>
+        <!-- END SIDEBAR & CONTENT -->
+      </div>
+    </div>
